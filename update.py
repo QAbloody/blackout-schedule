45ccc1ec14cfbe4d0ddadc6cb6079dd6975212e4
QAbloody <148036715+QAbloody@users.noreply.github.com>
2026-02-11 19:43:23 +0200

    Update update.py

--- update.py
+++ update.py
@@ -51,7 +51,7 @@ GROUP_ADDRESSES = {
 def setup_driver() -> webdriver.Chrome:
     """Налаштовує Chrome WebDriver"""
     options = Options()
-    options.add_argument("--headless")
+    #options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
